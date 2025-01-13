9ec6d5ed96c064cbf94d3a5a9d816ab6777152f5
Mertali Tosun <mertali2635@icloud.com>
2025-01-13 21:50:34 +0300

    customer index

--- customer-management/index.js
+++ customer-management/index.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
+
+const sequelize = require("../common/db/dbConnection");
+const handleError = require('../common/middlewares/errorHandler');
+
+require("../common/db/dbConnection");
+// require("./src/data/relationships");
+
+
+app.use(cors({origin: '*',credentials: true}));
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+
+
+// (async () => {
+//   await sequelize.sync({ force: true });
+// })();
+
+app.use(handleError);
 
 app.listen(process.env.PORT || 3003, () => {
   console.log(process.env.PORT || 3003, "portuna bağlandı");
