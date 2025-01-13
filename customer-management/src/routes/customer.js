5d20ab34cbbe7a6445faf3473964d58c726e2bc5
Mertali Tosun <mertali2635@icloud.com>
2025-01-13 23:53:25 +0300

    customer routes fix

--- customer-management/src/routes/customer.js
+++ customer-management/src/routes/customer.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const customerController = require("../controllers/customer");
 const {isAuth,isAdmin} = require("../../../common/middlewares/auth");
 
-router.delete("/customers/customerId",isAuth,isAdmin,customerController.delete_customers);
-router.get("/customers/customerId",isAuth,isAdmin,customerController.get_customer_byId);
+router.delete("/customers/:customerId",isAuth,isAdmin,customerController.delete_customers);
+router.get("/customers/:customerId",isAuth,isAdmin,customerController.get_customer_byId);
 router.patch("/customers/:customerId",isAuth,isAdmin,customerController.update_customers);
 router.post("/customers",isAuth,isAdmin,customerController.create_customers);
 router.get("/customers",isAuth,isAdmin,customerController.get_customers);
