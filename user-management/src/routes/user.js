73574bf7c410b30dfd9ac906f62af8404449157d
Mertali Tosun <mertali2635@icloud.com>
2025-01-13 22:55:18 +0300

    user microservices user route fix

--- user-management/src/routes/user.js
+++ user-management/src/routes/user.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const userController = require("../controllers/user");
 const {isAuth, isAdmin} = require("../../../common/middlewares/auth");
 
-router.get("/users",isAuth,userController.get_users);
-router.get("/users/:userId",isAuth,userController.get_user_byId);
-router.post("/users",isAuth,userController.create_users);
-router.patch("/users/:userId",isAuth,userController.update_user);
-router.delete("/users/:userId",isAuth,userController.delete_user);
+router.get("/users/:userId",isAuth,isAdmin,userController.get_user_byId);
+router.patch("/users/:userId",isAuth,isAdmin,userController.update_user);
+router.delete("/users/:userId",isAuth,isAdmin,userController.delete_user);
+router.post("/users",isAuth,isAdmin,userController.create_users);
+router.get("/users",isAuth,isAdmin,userController.get_users);
 module.exports = router;
