94aa5f2947d58f8a30112c90d80dd01f4b9d3b4f
Mertali Tosun <mertali2635@icloud.com>
2025-01-14 16:44:31 +0300

    api-gateway

---
+++ api-gateway/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const { createProxyMiddleware } = require("http-proxy-middleware");
+require("dotenv").config();
+
+const CUSTOMERS_SERVICE_URL = process.env.CUSTOMERS_SERVICE_URL;
+const SALES_SERVICE_URL = process.env.SALES_SERVICE_URL;
+const USERS_SERVICE_URL = process.env.USERS_SERVICE_URL;
+
+app.use("/users",createProxyMiddleware({target:USERS_SERVICE_URL, changeOrigin: true }));
+app.use("/customers",createProxyMiddleware({target:CUSTOMERS_SERVICE_URL, changeOrigin: true }));
+app.use("/sales",createProxyMiddleware({target:SALES_SERVICE_URL, changeOrigin: true }));
+app.use(express.json());
+
+app.listen(process.env.PORT || 3004, () => {
+    console.log(process.env.PORT || 3004, "portuna bağlandı. Api-gateway");
+  });
+  
